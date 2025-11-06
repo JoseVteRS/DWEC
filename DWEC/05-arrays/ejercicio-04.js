a490a898690d468f1efc27084202ea4f4c49ee14
Jose <jvrs.90@gmail.com>
2025-11-06 19:44:44 +0100

    Array: Ejercicio 04

---
+++ DWEC/05-arrays/ejercicio-04.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+
+const [primer, segundo, ...resto] = numeros;
+
+const restoNuemro = resto;
+
+console.log(primer);
+console.log(segundo);
+console.log(restoNuemro);
